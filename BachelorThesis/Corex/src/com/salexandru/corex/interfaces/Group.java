c34bc55747a9a962f8ad7dc893ff9729952b3337
Alexandru Stefanica <SAlexandru@users.noreply.github.com>
2015-02-05 08:59:37 +0200

    Update Group.java

--- BachelorThesis/Corex/src/com/salexandru/corex/interfaces/Group.java
+++ BachelorThesis/Corex/src/com/salexandru/corex/interfaces/Group.java
@@ -3,16 +3,34 @@ package com.salexandru.corex.interfaces;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.lang.reflect.InvocationHandler;
 
-public class Group <T extends XEntity> implements Iterable<T> {
+public class Group <T extends XEntity> implements InvocationHandler {
 	private List<T> elements_;
 	
 	public Group(List<T> elements) {
 		elements_ = new ArrayList<>(elements);
 	}
+	
+	public List<T> getElements() {return elements_;}
 
-	@Override
-	public Iterator<T> iterator() {
-		return elements_.iterator();
+	/**
+	 *   Must test this actually works and also if provides the benefits that I think it does
+	 */
+	public Object invoke(Object proxy, Method m, Object... args) throws Throwable {
+		// may useless ?
+		if (null == proxy || !(proxy instanceof Group<T extends XEntity>)) {
+			throw new IllegalArgumentException();
+		}
+		Object result;
+		try {
+			// we don't get into an infinit loop here ??
+			Group<T extends XEntity> p = (Group<T extends XEntity>)proxy;
+			result = m.invoke(p.getElements(), args);
+		}
+		catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+		return result;
 	}
 }
