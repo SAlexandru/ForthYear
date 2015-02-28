a2cbe65691bcd9e671025759b15fb18e26dbf7bb
SAlexandru <salexandru.alex@gmail.com>
2015-02-28 15:25:10 +0200

    restored history

---
+++ BachelorThesis/Corex/src/com/salexandru/corex/view/CorexView.java
@@ -0,0 +1,20 @@
+package com.salexandru.corex.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class CorexView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+        String mamaAreMere = "BA MERE";
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
